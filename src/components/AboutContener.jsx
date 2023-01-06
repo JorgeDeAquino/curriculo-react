cfa60927734fc2d5e9bf657a962fb73d3ad71827
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-01-06 12:45:41 -0300

    teste

--- src/components/AboutContener.jsx
+++ src/components/AboutContener.jsx
@@ -4,7 +4,7 @@ const AboutContainer = () => {
     return (
         <section className="about-container">
             <h2>Sobre</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, inventore eius, adipisci rem corrupti quibusdam nihil ratione voluptas cum eligendi eum quam ipsam molestias debitis voluptatem commodi voluptates aspernatur laboriosam.</p>
+            <p>Lorem  dolor sit amet consectetur adipisicing elit. Unde, inventore eius, adipisci rem corrupti quibusdam nihil ratione voluptas cum eligendi eum quam ipsam molestias debitis voluptatem commodi voluptates aspernatur laboriosam.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam eos magnam unde dolorem adipisci vel asperiores atque aspernatur facere, fuga possimus facilis deleniti dignissimos ad. Ducimus dolorum atque error maiores.</p>
         </section>
     )
