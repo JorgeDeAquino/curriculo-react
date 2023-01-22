18fae2e902f952623c686e1460e3a586077b2963
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-01-21 23:43:34 -0300

    alteracao de projects container

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -3,10 +3,10 @@
 const ProjectsContainer = () => {
     return (
         <section className="projects-container">
-            <h2>Projetos</h2>
+            <h2>Projeto em Destaque</h2>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur deserunt, et nisi sit, ullam corporis vero eaque optio, maiores aspernatur odio culpa animi? Sint quo, facilis sapiente hic nesciunt molestiae.</p>
             <a href="" className="btn">
-                Ver Projetos
+                Ver Projeto
             </a>
         </section>
     )
