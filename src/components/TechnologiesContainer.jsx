8c7a3f8512ee991e17774e43e549c01a26f39893
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-01-21 23:39:12 -0300

    alteracao  de layout base

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -35,7 +35,7 @@ const TechnologiesContainer = () => {
                     {tech.icon}
                     <div className="technology-info">
                         <h3>{tech.name}</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur.</p>
+
                     </div>
                 </a>
             ))}
