970e60f4a6b542e54b63181b18e151acb8addaf4
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-02-19 12:38:26 -0300

    ajuste projeto

--- src/mainContent/components/ProjectsContainer/index.js
+++ src/mainContent/components/ProjectsContainer/index.js
@@ -2,21 +2,23 @@ import React from "react";
 import { StyledProjectsContainer } from "./styles";
 
 export default function ProjectsContainer() {
-  return (
-    <StyledProjectsContainer>
-      <h2>Projeto em Destaque</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-        deserunt, et nisi sit, ullam corporis vero eaque optio, maiores
-        aspernatur odio culpa animi? Sint quo, facilis sapiente hic nesciunt
-        molestiae.
-      </p>
-      <a href="" className="btn">
-        Ver Projeto
-      </a>
-      <a href="" className="btn">
-        Ver Repositorio
-      </a>
-    </StyledProjectsContainer>
-  );
+    return (
+        <StyledProjectsContainer>
+            <h2>Projeto em Destaque</h2>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+                deserunt, et nisi sit, ullam corporis vero eaque optio, maiores
+                aspernatur odio culpa animi? Sint quo, facilis sapiente hic nesciunt
+                molestiae.
+            </p>
+            <div className="links">
+                <a href="" className="btn">
+                    Ver Projeto
+                </a>
+                <a href="" className="btn">
+                    Ver Repositorio
+                </a>
+            </div>
+        </StyledProjectsContainer>
+    );
 }
--- src/mainContent/components/ProjectsContainer/styles.js
+++ src/mainContent/components/ProjectsContainer/styles.js
@@ -2,9 +2,17 @@ import styled from "styled-components";
 
 export const StyledProjectsContainer = styled.div`
     margin-top: var(--margin-distance);
+
+    p {
+        margin-top: var(--margin-distance);
+        text-align: justify;
+    }
+    
+    .links{
+        margin-top: 2rem;
+    }
     
     .btn{
-        margin-top: var(--margin-distance);
         padding: 1rem 1.2rem;
         color: var(--text-color);
         font-size: var(--font-size-text);
@@ -12,5 +20,8 @@ export const StyledProjectsContainer = styled.div`
         text-decoration: none;
         border-radius: var(--border-radius);
         align-self: start;
+        &:first-child {
+            margin-right: 2rem;
+        }
     }    
 `;
