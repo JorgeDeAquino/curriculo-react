5adf26b2389d9a1af1b8d055d471e9272b924274
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-02-20 23:49:12 -0300

    alteracao no texto sobre

--- src/mainContent/components/AboutContainer/index.js
+++ src/mainContent/components/AboutContainer/index.js
@@ -5,10 +5,16 @@ export default function AboutContainer() {
     return (
         <StyledAboutContainer>
             <h2>Sobre</h2>
-            <p>Programador Front End ativo e habilidoso, com conhecimento relacionado a experiência do usuário e acessibilidade, utilizando os conceitos de Scrum para desenvolvimento de forma ágil.
+            <p>
+                Desenvolvedor Front End habilidoso e engajado, busco sempre melhorar a experiência do usuário e a acessibilidade em meus projetos. Adoto uma abordagem ágil, baseada no Scrum, para garantir que meus projetos sejam entregues com sucesso e dentro do prazo.
             </p>
-            <p>Atualmente trabalho com React, tenho experiência no desenvolvimento de projetos utilizando HTML, CSS e JavaScript. Geralmente utilizo o Sass para o pré-processamento do CSS e também os frameworks Bootstrap e Tailwind a depender da demanda e estilização do projeto.</p>
-            <p>Habilidades: autodidatismo, humanização para com a abordagem dos clientes, visão estratégica, adaptabilidade e expertise na solução de conflitos em equipes.</p>
+            <p>
+                Com expertise em React, tenho uma experiência no desenvolvimento de projetos utilizando HTML, CSS e JavaScript. Sempre estou à procura de novas ferramentas e técnicas para melhorar a qualidade do meu trabalho e entregar soluções eficientes e esteticamente agradáveis.
+
+            </p>
+            <p>
+                Além de meu conhecimento técnico, também valorizo minhas habilidades humanas, como autodidatismo, visão estratégica, adaptabilidade e capacidade de solucionar conflitos em equipe. Eu entendo que o sucesso de um projeto vai muito além do código escrito, e que a comunicação e empatia são fundamentais para o desenvolvimento de uma equipe forte e coesa.
+            </p>
         </StyledAboutContainer>
     )
 }
