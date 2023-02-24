484ccc2a51a2b6419b019ac58f6bca430f147cfe
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-02-23 22:19:16 -0300

    add links no social

--- src/sidebar/components/SocialNetworks/index.js
+++ src/sidebar/components/SocialNetworks/index.js
@@ -3,14 +3,14 @@ import { StyledSocialNetworks } from './styles';
 import {FaLinkedinIn, FaGithub} from 'react-icons/fa'
 
 const socialNetworks = [
-    { name: "linkedin", icon: <FaLinkedinIn/>},
-    { name: "github", icon: <FaGithub/>},
+    { name: "linkedin", icon: <FaLinkedinIn/>, destino: "https://www.linkedin.com/in/jorge-alexandre-de-aquino/"},
+    { name: "github", icon: <FaGithub/>, destino: "https://github.com/JorgeDeAquino"},
 ]
 
 export default function SocialNetworks() {
     return <StyledSocialNetworks>
     {socialNetworks.map((network) => (
-        <a href="#" className="social-btn" id={network.name} key={network.name}>
+        <a href={network.destino} target={'_blank'} className="social-btn" id={network.name} key={network.name}>
             {network.icon}
         </a>
     ))}
